607d453202c3c6ffa9f1a085362be41a103841bc
GhostDragon <jose061197@gmail.com>
2018-10-09 13:21:17 +0200

    ejemplo para lex
    he añadido tab, espacios y saltos de linea

---
+++ ejemplo.c
@@ -0,0 +1,36 @@
+
+booleano es_a_mayor_que_b(entero numero_a , entero numero_b){
+	booleno  salidas=falso;
+	
+	si (numero_a > numero_b){
+		
+		salidas=verdadero;
+		
+	}
+	
+	devolver salidas;
+}
+
+entero principal(){
+	
+	entero valor_a = 1000;
+	entero valor_b = 20;
+	entero suma= valor_a+valor_b;
+	
+	salida  suma;
+	
+	real a;
+	entrada a;
+	
+	mientras(a > 0){
+		
+		salida "El valor de a es " a;
+		a=a-1;
+	}
+	
+	caracter cadena='hola';
+	
+	salida es_a_mayor_que_b(valor_a ,valor_b);
+	
+	
+}
